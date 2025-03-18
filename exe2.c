68a86f8e7dd9ebb5106e541c80851b65aec8b4b9
sartori03 <marcolinosartori03@gmail.com>
2025-03-18 20:17:52 -0300

    exe2.c

--- exe2.c
+++ exe2.c
@@ -3,7 +3,7 @@
 int main() { 
   int ano = 2006;
   int idade = 2025 - ano;
-  ;printf("Nascimento: %d\n", ano);
-  ;printf("Idade: %d", idade);
+  printf("Nascimento: %d\n", ano);
+  printf("Idade: %d", idade);
     return 0;
 } 
